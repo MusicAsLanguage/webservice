ba112aedc4699020d6a51475ba858ebe29a6c0dc
Shengyu Fu <shengyfu@microsoft.com>
2021-05-26 00:02:25 -0700

    Adding token refresh API

--- resources/auth.py
+++ resources/auth.py
@@ -1,10 +1,11 @@
 from flask import Response, request
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from database.models import User
 from flask_restful import Resource
 from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist
 from resources.errors import SchemaValidationError, EmailAlreadyExistsError, UnauthorizedError, InternalServerError
 import datetime
+import json
 
 class SignupApi(Resource):
     def post(self):
@@ -34,14 +35,29 @@ class LoginApi(Resource):
             if not authorized:
                 raise UnauthorizedError
 
-            expires = datetime.timedelta(days=7)                       
-            access_token = create_access_token(identity = activeUser.to_json(), expires_delta = expires)
-            return {'token': access_token}, 200
+            expires = datetime.timedelta(days=7)
+            activeUser.password = ""                       
+            access_token = create_access_token(identity = activeUser.to_json(), expires_delta = expires, fresh = True)            
+            refresh_token = create_refresh_token(str(activeUser.id))
+            return {'token': access_token, 'refresh_token': refresh_token}, 200
         except (UnauthorizedError, DoesNotExist):
             raise UnauthorizedError
         except Exception as e:
             raise InternalServerError
-            
+
+class TokenRefreshApi(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        try:
+            # retrive the user's identity from the refresh token using a Flask-JWT-Extended built-in method    
+            userId = get_jwt_identity() 
+            user = User.objects.get(id=userId)
+            user.password = "" 
+            # return a non-fresh token for the user
+            new_token = create_access_token(identity=user.to_json(), fresh=False)
+            return {'token': new_token}, 200
+        except Exception as e:
+            raise InternalServerError  
             
             
             
--- resources/routes.py
+++ resources/routes.py
@@ -1,9 +1,10 @@
-from .auth import SignupApi, LoginApi
+from .auth import SignupApi, LoginApi, TokenRefreshApi
 from .lessons import GetLessonsApi, CreateLessonsApi, GetActivityStatusApi, UpdateActivityStatusApi
 
 def initialize_routes(api):    
     api.add_resource(SignupApi, '/api/auth/signup')
     api.add_resource(LoginApi, '/api/auth/login')
+    api.add_resource(TokenRefreshApi, '/api/auth/tokenRefresh')
     api.add_resource(GetLessonsApi, '/api/lesson/getLessons')
     api.add_resource(CreateLessonsApi, '/api/lesson/createLessons')
     api.add_resource(GetActivityStatusApi, '/api/activity/getStatus')
